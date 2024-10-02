0629fdf6707e283649fe543905076c1ce52e64d9
Rene Lazalde <rene.lazalde@gmail.com>
2024-10-02 12:12:01 -0600

    Tests

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
+    output: 'export',
     experimental: {
       optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
     },
